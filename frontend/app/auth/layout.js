2db790656bcf3292188d2911a8a2e08c13048000
Md. Masud Mazumder <mdmasud.csecu@gmail.com>
2024-03-25 13:25:54 +0600

    frontend problem solved

--- frontend/app/auth/layout.js
+++ frontend/app/auth/layout.js
@@ -1,44 +1,32 @@
-"use client"
+"use client";
 
-import { Inter } from "next/font/google";
-import { Providers } from "../providers";
 import { Box } from "@mui/material";
 import { useEffect, useState } from "react";
-import './auth.css'
-
-
-const inter = Inter({ subsets: ["latin"] });
-
+import "./auth.css";
 
 export default function RootLayout({ children }) {
-
-	const [bgImage, setbgImage] = useState('/assets/1.jpg')
-
+	const [bgImage, setbgImage] = useState("/assets/1.jpg");
 
 	useEffect(() => {
-		let num = 1
+		let num = 1;
 		setInterval(() => {
-		  setbgImage('/assets/'+num+'.jpg')
-		  num = num + 1;
-		  if(num==10) num = 1;
-		}, 3000)
-	  }, []);
-
+			setbgImage("/assets/" + num + ".jpg");
+			num = num + 1;
+			if (num == 10) num = 1;
+		}, 3000);
+	}, []);
 
 	return (
-		<html lang="en">
-			<body className={inter.className}>
-				<Providers>
-					<Box className={'main'}>
-						<Box className='absoluteBg text-foreground flexMiddle' >
-							<img className='img' src={bgImage} draggable={false} alt="background-image"/>
-							{children}
-						</Box>
-						
-					</Box>
-				</Providers>
-			</body>
-		</html>
+		<Box className={"main"}>
+			<Box className="absoluteBg text-foreground flexMiddle">
+				<img
+					className="img"
+					src={bgImage}
+					draggable={false}
+					alt="background-image"
+				/>
+				{children}
+			</Box>
+		</Box>
 	);
 }
-
