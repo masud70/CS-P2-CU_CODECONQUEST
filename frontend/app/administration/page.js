c99af56ec064c34bece4eb0f906c8c9f7ad4a485
Md. Masud Mazumder <mdmasud.csecu@gmail.com>
2024-03-26 13:04:05 +0600

    Admin layout and header modified

--- frontend/app/administration/page.js
+++ frontend/app/administration/page.js
@@ -1,32 +1,32 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
-import './administration.css'
-import Drawer from '@mui/material/Drawer';
-import { Box } from '@mui/material';
+import React, { useState } from "react";
+import "./administration.css";
+import Drawer from "@mui/material/Drawer";
+import { Box } from "@mui/material";
 
-import { TiThMenu  } from "react-icons/ti";
-import DrawerList from './components/DrawerList/DrawerList';
-import UserManagementTab from './components/Tabs/UserManagementTab/UserManagementTab';
+import { TiThMenu } from "react-icons/ti";
+import DrawerList from "./components/DrawerList/DrawerList";
+import UserManagementTab from "./components/Tabs/UserManagementTab/UserManagementTab";
 
 export default function Page() {
-    
-    const [sideBarShow, setsideBarShow] = useState(true)
+	const [sideBarShow, setsideBarShow] = useState(false);
 
-    const toggleDrawer = () => () => {
-        setsideBarShow(!sideBarShow);
-      };
-    
+	const toggleDrawer = () => () => {
+		setsideBarShow(!sideBarShow);
+	};
 
-    return (
-        <Box className='AdminPageDiv'>
-            <Box className={'hamBar'} onClick={toggleDrawer()}><TiThMenu  size={'1.5em'} /></Box>
+	return (
+		<Box className="AdminPageDiv">
+			<Box className={"hamBar"} onClick={toggleDrawer()}>
+				<TiThMenu size={"1.5em"} />
+			</Box>
 
-            <UserManagementTab/>
+			<UserManagementTab />
 
-            <Drawer open={sideBarShow} onClose={toggleDrawer()}>
-                <DrawerList toggleDrawer={toggleDrawer}/>
-            </Drawer>
-        </Box>
-    )
+			<Drawer open={sideBarShow} onClose={toggleDrawer()}>
+				<DrawerList toggleDrawer={toggleDrawer} />
+			</Drawer>
+		</Box>
+	);
 }
