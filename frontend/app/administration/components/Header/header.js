032cc2a6ac5d91fa520c745456562eb7138be28c
Brainless-Loco <dev.tcdas@gmail.com>
2024-03-26 20:09:58 +0600

    UserManagement Basic UI setup done

--- frontend/app/administration/components/Header/header.js
+++ frontend/app/administration/components/Header/header.js
@@ -1,37 +1,17 @@
 import React from "react";
-// import "./header.css";
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { Button } from "@nextui-org/react";
 import { UserIcon } from "./UserIcon";
 
 export default function Header() {
 	return (
-		<>
-			<div className="w-full min-h-[60px] bg-sky-300 flex justify-between items-center">
-				<div className="w-[25%] text-center"></div>
-				<div className="w-[50%] items-center text-center font-bold text-2xl">
-					System Administration
-				</div>
-				<div className="w-[25%] flex items-center justify-center">
-					<Box className="">
-						<Button
-							color="danger"
-							variant="bordered"
-							startContent={<UserIcon />}
-						>
-							Log Out
-						</Button>
-					</Box>
-				</div>
-			</div>
-			{/* <Box className={"menuDiv"}>
-				<Box className={["alignLeft", "menuCol"]}></Box>
-				<Box className={["alignCenter", "menuCol"]}>
-					<Typography sx={{ fontWeight: "bold" }} variant="h5">
-						System Administration
-					</Typography>
-				</Box>
-				<Box className={["alignRight", "menuCol"]}>
+		<Box className="w-full min-h-[60px] bg-sky-300 flex justify-between items-center px-4">
+			<Box className="w-[25%] text-center"></Box>
+			<Box className="w-[50%] text-center font-bold text-2xl text-cyan-800">
+				System Administration
+			</Box>
+			<Box className="w-[25%] flex justify-end">
+				<Box className="">
 					<Button
 						color="danger"
 						variant="bordered"
@@ -40,7 +20,7 @@ export default function Header() {
 						Log Out
 					</Button>
 				</Box>
-			</Box> */}
-		</>
+			</Box>
+		</Box>
 	);
 }
