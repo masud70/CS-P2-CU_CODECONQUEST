dad92232de89013dfdd89725c5b723dbc80b5828
Md. Masud Mazumder <mdmasud.csecu@gmail.com>
2024-03-27 23:12:06 +0600

    Profile done and a simpler layout added

--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-	return <div>User Dashboard</div>;
+	return (
+		<div className="w-full bg-green-200 h-screen justify-center text-center items-center flex flex-col space-y-3">
+			<div className="font-bold text-3xl">This is common Dashboard</div>
+			<div className="font-bold w-[220px] text-2xl border-2 rounded border-green-400 px-2 bg-fuchsia-200">
+				<Link href="/dashboard/admin">System Admin</Link>
+			</div>
+			<div className="font-bold w-[220px] text-2xl border-2 rounded border-green-400 px-2 bg-fuchsia-200">
+				<Link href="/dashboard/sts">STS Manager</Link>
+			</div>
+			<div className="font-bold w-[220px] text-2xl border-2 rounded border-green-400 px-2 bg-fuchsia-200">
+				<Link href="/dashboard/landfill">Landfill Manager</Link>
+			</div>
+		</div>
+	);
 };
 
 export default page;
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,10 +1,32 @@
+import { Box } from "@mui/material";
 import React from "react";
+import Link from "next/link";
 
 export default function Home() {
 	return (
 		<>
-			<div className="font-bold text-5xl items-center w-screen h-screen justify-center flex">
-				Code Samurai 2024
+			<div className="font-bold items-center w-screen h-screen justify-center flex">
+				<Box className={"main absolute z-0"}>
+					<Box className="absoluteBg text-foreground flexMiddle">
+						<img
+							className="img"
+							src="/assets/ecosync.png"
+							draggable={false}
+							alt="background-image"
+						/>
+					</Box>
+				</Box>
+				<div className="z-10 text-center items-center justify-center flex space-y-2 flex-col mt-64">
+					<div className="font-extrabold text-6xl text-slate-900">
+						Code Samurai 2024
+					</div>
+					<div className="font-extrabold text-4xl text-slate-900">
+						Team: CU_CODECONQUEST
+					</div>
+					<div className="text-xl font-bold rounded-xl border-2 transition-colors bg-green-500 overflow-hidden hover:text-white border-solid hover:bg-green-800 w-32 p-2">
+						<Link href="/auth/login">Login</Link>
+					</div>
+				</div>
 			</div>
 		</>
 	);
