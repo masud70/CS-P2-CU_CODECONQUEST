3d9524702ef3e1148c91c20f64368a011d271a06
Md. Masud Mazumder <mdmasud.csecu@gmail.com>
2024-03-27 16:34:27 +0600

    STS manager ui

--- frontend/app/providers.jsx
+++ frontend/app/providers.jsx
@@ -3,8 +3,18 @@
 
 import store from "@/redux/store/store";
 import { NextUIProvider } from "@nextui-org/react";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 export function Providers({ children }) {
-	return <NextUIProvider><Provider store={store}>{children}</Provider></NextUIProvider>;
+	return (
+		<NextUIProvider>
+			<Provider store={store}>
+				<LocalizationProvider dateAdapter={AdapterDayjs}>
+					{children}{" "}
+				</LocalizationProvider>
+			</Provider>
+		</NextUIProvider>
+	);
 }
