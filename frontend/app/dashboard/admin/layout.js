791b32417c5daa374eb3b5c301a650b5f41a84a2
Atanukumardey <atanudey2153@gmail.com>
2024-03-31 20:22:42 +0600

    Update UI styles and fix bugs from Brainless

--- frontend/app/dashboard/admin/layout.js
+++ frontend/app/dashboard/admin/layout.js
@@ -7,6 +7,7 @@ import MenuCard from "@/components/MenuCard";
 import Truck from "@/icon/Truck";
 import Home from "@/icon/Home";
 import StsIcon from "@/icon/StsIcon";
+import { RiProfileFill } from "react-icons/ri";
 
 const layout = ({ children }) => {
 	const router = useRouter();
@@ -23,34 +24,34 @@ const layout = ({ children }) => {
 	}, [auth]);
 
 	return (
-		<div className="w-full lg:w-4/5 h-full flex flex-row rounded-b overflow-hidden">
+		<div style={{backgroundColor:'#eaf8ef'}} className="w-full lg:w-4/5 h-full flex flex-row rounded-b overflow-hidden">
 			<div className="w-1/3 lg:w-1/5 h-full p-2 bg-slate-200 flex flex-col space-y-2 overflow-auto">
-				<MenuCard
-					className="bg-purple-500"
+				<MenuCard  
+					text="User Management"
+					icon={<RiProfileFill size={70}/>}
+					link="/administration/user"
+				/>
+				<MenuCard 
 					text="Home"
 					icon={<Home />}
 					link="/dashboard/admin"
 				/>
-				<MenuCard
-					className="bg-blue-500"
+				<MenuCard 
 					text="Vehicle Entry"
 					icon={<Truck />}
 					link="/dashboard/admin/vehicle-register"
 				/>
-                <MenuCard
-					className="bg-yellow-500"
+				<MenuCard 
 					text="Vehicle Assignment"
 					icon={<StsIcon />}
 					link="/dashboard/admin/vehicle-assignment"
 				/>
-				<MenuCard
-					className="bg-red-500"
+				<MenuCard 
 					text="STS Register"
 					icon={<StsIcon />}
 					link="/dashboard/admin/sts-register"
 				/>
-                <MenuCard
-					className="bg-green-500"
+				<MenuCard 
 					text="STS Management"
 					icon={<StsIcon />}
 					link="/dashboard/admin/sts-management"
--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -1,19 +1,24 @@
+import { Box } from "@mui/material";
 import Link from "next/link";
 import React from "react";
+import './dashboard.css'
 
 const page = () => {
 	return (
-		<div className="w-full bg-green-200 h-screen justify-center text-center items-center flex flex-col space-y-3">
-			<div className="font-bold text-3xl">This is common Dashboard</div>
-			<div className="font-bold w-[220px] text-2xl border-2 rounded border-green-400 px-2 bg-fuchsia-200">
-				<Link href="/dashboard/admin">System Admin</Link>
-			</div>
-			<div className="font-bold w-[220px] text-2xl border-2 rounded border-green-400 px-2 bg-fuchsia-200">
-				<Link href="/dashboard/sts">STS Manager</Link>
-			</div>
-			<div className="font-bold w-[220px] text-2xl border-2 rounded border-green-400 px-2 bg-fuchsia-200">
-				<Link href="/dashboard/landfill">Landfill Manager</Link>
-			</div>
+		<div className="w-[80%] h-screen justify-center text-center items-center flex flex-col space-y-3">
+			
+			<Box className="flex flex-row justify-evenly w-full">
+				<div className="aSingleDiv">
+					<Link href="/dashboard/admin">System Admin</Link>
+				</div>
+				<div className="aSingleDiv">
+					<Link href="/dashboard/sts">STS Manager</Link>
+				</div>
+				<div className="aSingleDiv">
+					<Link href="/dashboard/landfill">Landfill Manager</Link>
+				</div>
+			</Box>
+			
 		</div>
 	);
 };
