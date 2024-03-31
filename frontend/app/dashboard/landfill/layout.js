92d94f246a00341c4cb589ebcee68d023b100ede
Atanukumardey <atanudey2153@gmail.com>
2024-03-31 21:01:42 +0600

    Refactor CSS styles and update button class

--- frontend/app/dashboard/landfill/layout.js
+++ frontend/app/dashboard/landfill/layout.js
@@ -26,19 +26,16 @@ const layout = ({ children }) => {
 		<div className="w-full lg:w-4/5 h-full flex flex-row rounded-b overflow-hidden">
 			<div className="w-1/3 lg:w-1/5 h-full p-2 bg-slate-200 flex flex-col space-y-2 overflow-auto">
 				<MenuCard
-					className="bg-purple-500"
 					text="Home"
 					icon={<Home />}
 					link="/dashboard/landfill"
 				/>
 				<MenuCard
-					className="bg-blue-500"
 					text="Vehicle Entry"
 					icon={<Truck />}
 					link="/dashboard/landfill/vehicle-entry"
 				/>
 				<MenuCard
-					className="bg-red-500"
 					text="Bill"
 					icon={<Taka />}
 					link="/dashboard/landfill/bill"
