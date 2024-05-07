31a21e0b0257c230d70b80bbc976e7a7b6d8a2a8
Md. Masud Mazumder <mdmasud.csecu@gmail.com>
2024-05-07 12:01:24 +0600

    Basic mobile app setup

---
+++ EcoSync/app/index.js
@@ -0,0 +1,23 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<Text>Hello World!</Text>
+			<StatusBar style="auto" />
+			<Link href="/dashboard">Dashboard</Link>
+			<Link href="/admin">Admin</Link>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
